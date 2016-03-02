05450c64b9230b1de5afb9fabdbcd2d514ba0df0
mPemba <michael.j.say@gmail.com>
2016-03-01 18:46:01 -0700

    third commit

--- public/app.js
+++ public/app.js
@@ -1,1 +1,32 @@
-var scotchTodo = angular.module('bcwDayOne', []);
+var app = angular.module('bcwDayOne', []);
+
+function mainController() {
+  var count = 0;
+  $('.upButton').on('click', function() {
+    count += 1;
+
+    $('#count').html(count);
+  });
+
+  $('.leapButton').on('click', function() {
+    getAge("19900901");
+  });
+
+
+
+  function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+    $('#leapYear').html(age);
+    return age;
+    }
+
+
+
+}
